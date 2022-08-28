e11b57d33ff5193a8d29d515366b612fa15453c6
John J Setlock II <johnsetlock@gmail.com>
2022-08-28 11:48:22 -0500

    commit tree 204ad68ebb9492b06dfb3aa08246a0ddae973285

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,31 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown(number=10)
+    
+   
+    while number>0 do
+        puts "#{number} SECOND(S)!"
+        
+        number-=1
+    end
+    
+    
+    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number=5)
+    
+   
+    while number>0 do
+        puts "#{number} SECOND(S)!"
+        sleep(1)
+        number-=1
+    end
+    
+    
+    return "HAPPY NEW YEAR!"
+end
+
+
+binding.pry
